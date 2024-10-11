2bfaac3fa20b71601538dade6e6ee1adbbe5cf80
adrieeltorres1 <adrieltorreslds@gmail.com>
2024-10-10 23:44:00 -0300

    fix: switch case

---
+++ js/index.js
@@ -0,0 +1,40 @@
+btn.onclick = function(){
+    switch(Number(mes.value)){
+        case 1:
+            console.log("bugado(a);");
+        break;
+        case 2:
+            console.log("do CTRL C, CTRL V;");
+        break;
+        case 3:
+            console.log("das gambiarras;");
+        break;
+        case 4:
+            console.log("que culpa o cache;");
+        break;
+        case 5:
+            console.log("que esquece o que faz;");
+        break;
+        case 6:
+            console.log("do git vazio;");
+        break;
+        case 7:
+            console.log("das try/catch vazia;");
+        break;
+        case 8:
+            console.log("famosinho do linkedin;");
+        break;
+        case 9:
+            console.log("caçador de bugs;");
+        break;
+        case 10:
+            console.log("do windows pirata;");
+        break;
+        case 11:
+            console.log("do update sem where;");
+        break;
+        case 12:
+            console.log("do commit bugado;");
+        break;
+    }
+}
